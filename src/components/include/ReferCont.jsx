4dfd03a911eaa31fb6504228d93ce6e16f4da8cf
Kim-chanmi <chanmi1320@gmail.com>
2022-11-06 22:36:21 +0900

    2022.11.06

--- src/components/include/ReferCont.jsx
+++ src/components/include/ReferCont.jsx
@@ -13,19 +13,6 @@ const ReferText = ({ num, title, desc, descStar }) => {
   );
 };
 
-const HtmlReferText = ({ num, title, desc, descStar }) => {
-  return (
-    <li>
-      <a href="/">
-        <span className="num">{num}</span>
-        <span className="name">{title}</span>
-        <span className="desc">{desc}</span>
-        <span className="star">{descStar}</span>
-      </a>
-    </li>
-  );
-};
-
 const ReferCont = ({ references }) => {
   return (
     <section className="cont__refer">
@@ -35,18 +22,6 @@ const ReferCont = ({ references }) => {
           <ul className="refer__list">
             {references.map((refer, idx) => (
               <ReferText
-                key={idx}
-                num={refer.num}
-                title={refer.title}
-                desc={refer.desc}
-                descStar={refer.descStar}
-              />
-            ))}
-          </ul>
-          <h2>html</h2>
-          <ul className="refer__list">
-            {references.map((refer, idx) => (
-              <HtmlReferText
                 key={idx}
                 num={refer.num}
                 title={refer.title}
