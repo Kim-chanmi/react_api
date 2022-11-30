bb9967f1b0f691268bd5dc5a6ba59c0912cadb31
Kim-chanmi <chanmi1320@gmail.com>
2022-11-30 12:28:59 +0900

    2022.11.30

--- src/components/include/Maincont.jsx
+++ src/components/include/Maincont.jsx
@@ -1,3 +1,32 @@
+// import React from "react";
+
+// const mainInfo = [
+//   { text: "we provide" },
+//   { text: "visual coding" },
+//   { text: "solutions" },
+//   { text: "for you API" },
+// ];
+
+// const MainText = ({ text }) => {
+//   return <div>{text}</div>;
+// };
+
+// const MainCont = () => {
+//   return (
+//     <section className="cont__main">
+//       <div className="container">
+//         <div className="main__inner">
+//           {mainInfo.map((text, idx) => (
+//             <MainText key={idx} text={text.text} />
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default MainCont;
+
 import React from "react";
 
 const mainInfo = [
@@ -6,7 +35,6 @@ const mainInfo = [
   { text: "solutions" },
   { text: "for you API" },
 ];
-
 const MainText = ({ text }) => {
   return <div>{text}</div>;
 };
@@ -16,8 +44,8 @@ const MainCont = () => {
     <section className="cont__main">
       <div className="container">
         <div className="main__inner">
-          {mainInfo.map((text, idx) => (
-            <MainText key={idx} text={text.text} />
+          {mainInfo.map((text, index) => (
+            <MainText key={index} text={text.text} />
           ))}
         </div>
       </div>
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import Header from "../layout/Header";
-// import Contents from "../layout/Contents";
-// import Footer from "../layout/Footer";
-// import MainCont from "../include/MainCont";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
